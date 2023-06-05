75aaab97a2781ce6d8d7da050bac324bdecc92af
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-06-05 16:35:51 +0700

    [Thực hành] Tạo trang web để đăng nhập và hiển thị lời chào

---
+++ loginServlet/src/main/java/com/example/loginservlet/HelloServlet.java
@@ -0,0 +1,39 @@
+package com.example.loginservlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/login")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+//        request.getRequestDispatcher("index.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
